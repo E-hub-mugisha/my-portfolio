815811b6d75f8467ada287a9f716a0b51519b92e
mugisha erik <kabosierik@gmail.com>
2023-11-25 20:12:07 +0200

    // //In this code, the comments provide a clear and concise explanation of the changes made in the commit. The comments are also focused on the important aspects of the commit, such as the files modified and the branch status. This approach ensures that the comments are informative and relevant to the commit, which is crucial for understanding the commit history and maintaining the project..</s>

--- src/data.js
+++ src/data.js
@@ -4,31 +4,31 @@ export const projects = [
     subtitle: "MERN Stack",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./assets/img/portfolio/rwandancda.png",
+    image: "./assets/img/portfolio/rncda.png",
     link: "https://www.rwandancda.org/",
   },
   {
-    title: "React Tracks",
+    title: "Blank Mavin",
     subtitle: "React and Python",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://github.com/E-hub-mugisha",
+    image: "./assets/img/portfolio/blank.png",
+    link: "https://blank.rw",
   },
   {
-    title: "DevChat",
+    title: "Homiez",
     subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://github.com/E-hub-mugisha",
+    image: "./assets/img/portfolio/homiez.png",
+    link: "https://homiez.rw",
   },
   {
-    title: "Epic Todo App",
+    title: "connector",
     subtitle: "React Hooks",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
+    image: "./assets/img/portfolio/connector.png",
     link: "https://github.com/E-hub-mugisha",
   },
 ];
@@ -57,4 +57,25 @@ export const skills = [
   "Node",
   "GraphQL",
   "Material UI",
+];
+
+export const clients = [
+  {
+    image: "https://www.homiez.rw/assets/img/favicon.png",
+    name: "homiez",
+    link: "https://www.homiez.rw"
+  },
+  {
+    image: "https://www.rwandancda.org/img/logo.png",
+    name: "rwandancda",
+    link: "https://www.rwandancda.org"
+  },
+  {
+    image: "https://www.homiez.rw/assets/img/favicon.png",
+    name: "blank"
+  },
+  {
+    image: "https://www.homiez.rw/assets/img/favicon.png",
+    name: "homiez"
+  }
 ];
