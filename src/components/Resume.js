f084f20d4486b174d052b380cf0fbd19d2547be1
mugisha erik <54761531+E-hub-mugisha@users.noreply.github.com>
2024-12-27 13:58:20 +0200

    style changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,34 +3,13 @@ import React from 'react'
 export const Resume = () => {
     return (
         <div>
-
-
-            <div class="page-title ">
-                <div class="heading">
-                    <div class="container">
-                        <div class="row d-flex justify-content-center text-center">
-                            <div class="col-lg-8">
-                                <h1>Resume</h1>
-                                <p class="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <nav class="breadcrumbs">
-                    <div class="container">
-                        <ol>
-                            <li><a href="index.html">Home</a></li>
-                            <li class="current">Resume</li>
-                        </ol>
-                    </div>
-                </nav>
-            </div>
-
-
             <section id="resume" class="resume section">
 
                 <div class="container">
-
+                    <div class="section-header">
+                        <h2>what shaped me</h2>
+                        <p>Resume</p>
+                    </div>
                     <div class="row">
 
                         <div class="col-lg-6 " >
--- src/components/Services.js
+++ src/components/Services.js
@@ -2,44 +2,45 @@ import React from 'react'
 
 export const Services = () => {
   return (
+    
     <main id="main" data-aos="fade" data-aos-delay="1500">
 
-    <section id="services" class="services">
-      <div class="container">
-      <div class="section-header">
-          <h2>what I do</h2>
-          <p>Services</p>
+      <section id="services" class="services">
+        <div class="container">
+          <div class="section-header">
+            <h2>what I do</h2>
+            <p>Services</p>
+          </div>
+          <div class="row gy-4">
+
+            <div class="col-xl-3 col-md-6 d-flex">
+              <div class="service-item position-relative">
+                <i class="bi bi-activity"></i>
+                <h4><a href="" class="stretched-link">web development</a></h4>
+                {/* <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p> */}
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 d-flex">
+              <div class="service-item position-relative">
+                <i class="bi bi-bounding-box-circles"></i>
+                <h4><a href="" class="stretched-link">Hosting services</a></h4>
+                {/* <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p> */}
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 d-flex">
+              <div class="service-item position-relative">
+                <i class="bi bi-calendar4-week"></i>
+                <h4><a href="" class="stretched-link">Data analysis</a></h4>
+                {/* <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p> */}
+              </div>
+            </div>
+
+          </div>
+
         </div>
-        <div class="row gy-4">
-
-          <div class="col-xl-3 col-md-6 d-flex">
-            <div class="service-item position-relative">
-              <i class="bi bi-activity"></i>
-              <h4><a href="" class="stretched-link">web development</a></h4>
-              {/* <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p> */}
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex">
-            <div class="service-item position-relative">
-              <i class="bi bi-bounding-box-circles"></i>
-              <h4><a href="" class="stretched-link">Hosting services</a></h4>
-              {/* <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p> */}
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex">
-            <div class="service-item position-relative">
-              <i class="bi bi-calendar4-week"></i>
-              <h4><a href="" class="stretched-link">Data analysis</a></h4>
-              {/* <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p> */}
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section>
+      </section>
     </main>
   )
 }
