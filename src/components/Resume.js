83d5bfbc5b664e8c8203fecbcafc977319925497
mugisha erik <54761531+E-hub-mugisha@users.noreply.github.com>
2024-12-27 14:20:06 +0200

    resume update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,70 +7,64 @@ export const Resume = () => {
 
                 <div class="container">
                     <div class="section-header">
-                        <h2>what shaped me</h2>
+                        <h2>Experience</h2>
                         <p>Resume</p>
                     </div>
                     <div class="row">
-
-                        <div class="col-lg-6 " >
-                            <h3 class="resume-title">Sumary</h3>
-
+                        <div class="col-lg-6">
+                            <h3 class="resume-title">Summary</h3>
                             <div class="resume-item pb-0">
-                                <h4>Brandon Johnson</h4>
-                                <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+                                <h4>Eric Mugisha</h4>
+                                <p><em>A highly motivated and detail-oriented computer scientist passionate about web development and IT management. Proficient in HTML, CSS, JavaScript, React, Angular, Laravel, PHP, and MySQL. Adept at technical support, networking, and IT procurement, with strong leadership and communication skills. Committed to continuous learning and contributing to a dynamic, forward-thinking team.</em></p>
                                 <ul>
-                                    <li>Portland par 127,Orlando, FL</li>
-                                    <li>(123) 456-7891</li>
-                                    <li>alice.barkley@example.com</li>
+                                    <li>Kigali, Rwanda</li>
+                                    <li>+(250) 782-390-919</li>
+                                    <li>kericmugisha@gmail.com</li>
+                                    <li><a href="https://www.linkedin.com/in/mugisha-eric-411547135/">LinkedIn</a></li>
+                                    <li><a href="https://ericmugisha.netlify.app/">Portfolio</a></li>
                                 </ul>
                             </div>
-
                             <h3 class="resume-title">Education</h3>
                             <div class="resume-item">
-                                <h4>Master of Fine Arts &amp; Graphic Design</h4>
-                                <h5>2015 - 2016</h5>
-                                <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                                <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+                                <h4>Bachelor’s in Computer Science</h4>
+                                <h5>2016 - 2021</h5>
+                                <p><em>University of Rwanda, Nyarugenge, Kigali, Rwanda</em></p>
+                                <p>Studied machine learning, relational databases, and web development, gaining expertise in web design, front-end/back-end development, networking, and web security.</p>
                             </div>
-
-                            <div class="resume-item">
-                                <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-                                <h5>2010 - 2014</h5>
-                                <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                                <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
-                            </div>
-
                         </div>
-
                         <div class="col-lg-6">
                             <h3 class="resume-title">Professional Experience</h3>
                             <div class="resume-item">
-                                <h4>Senior graphic design specialist</h4>
-                                <h5>2019 - Present</h5>
-                                <p><em>Experion, New York, NY </em></p>
+                                <h4>Microsoft Dynamics 365 ERP Technical Support Engineer</h4>
+                                <h5>May 2023 - Present</h5>
+                                <p><em>Tek Experts, Kigali City, Rwanda</em></p>
                                 <ul>
-                                    <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-                                    <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-                                    <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-                                    <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+                                    <li>Provided technical support for Microsoft Dynamics 365 Finance and Operations, resolving client inquiries and optimizing platform performance.</li>
+                                    <li>Conducted data analysis to identify and implement solutions for software discrepancies.</li>
+                                    <li>Maintained detailed case records and technical knowledge articles while communicating proactively with clients.</li>
                                 </ul>
                             </div>
                             <div class="resume-item">
-                                <h4>Graphic design specialist</h4>
-                                <h5>2017 - 2018</h5>
-                                <p><em>Stepping Stone Advertising, New York, NY</em></p>
+                                <h4>Full Stack Web Developer & IT Manager</h4>
+                                <h5>Dec 2021 - April 2023</h5>
+                                <p><em>HOMIEZ LTD, Kigali, Rwanda</em></p>
                                 <ul>
-                                    <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-                                    <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                                    <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                                    <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+                                    <li>Developed and maintained web applications tailored to client needs.</li>
+                                    <li>Collaborated with stakeholders to deliver user-friendly websites and troubleshoot technical issues.</li>
+                                    <li>Implemented IT policies, ensuring data security and system integrity.</li>
                                 </ul>
                             </div>
-
+                            <div class="resume-item">
+                                <h4>MIS Specialist Intern</h4>
+                                <h5>July 2019 - October 2019</h5>
+                                <p><em>Local Administrative Entities Development Agency (LODA), Gasabo, Kigali, Rwanda</em></p>
+                                <ul>
+                                    <li>Designed and maintained management information systems for organizational decision-making.</li>
+                                    <li>Provided technical support and ensured data accuracy across systems.</li>
+                                </ul>
+                            </div>
                         </div>
-
                     </div>
-
                 </div>
 
             </section>
